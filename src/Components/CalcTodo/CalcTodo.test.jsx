5e87179c7209352186029e1ecd93ec1ac134754d
Бороздин Дмитрий <borozdin1@yandex.ru>
2021-02-21 11:42:04 +0700

    Добавлен 1ый тест

--- src/Components/CalcTodo/CalcTodo.test.jsx
+++ src/Components/CalcTodo/CalcTodo.test.jsx
@@ -3,7 +3,15 @@ import {shallow} from 'enzyme';
 import CalcTodo from "./CalcTodo";
 
 test('case', ()=> {
-  const wrapper = shallow(<CalcTodo/>);
+  const props = {
+    tasks: [
+      {value:"Изучить React", isDone:false, isImportant: true, id:1},
+      {value:"Найти работу",isDone:false, isImportant: true,id:2}
+    ],
+    onClickFilteredTasks:'Все',
+    filtered:'Все'
+  };
 
+  const wrapper = shallow(<CalcTodo tasks={props.tasks}/>);
   expect(wrapper.exists()).toBe(true);
 });
