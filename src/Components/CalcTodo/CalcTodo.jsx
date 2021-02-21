59c5866c3eb866022952108d9dc2649a2f6ed882
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2021-02-21 15:38:02 +0700

    Добавлены новые тесты

--- src/Components/CalcTodo/CalcTodo.jsx
+++ src/Components/CalcTodo/CalcTodo.jsx
@@ -10,46 +10,45 @@ const CalcTodo = ({ tasks, onClickFilteredTasks, filtered }) => {
 
     return (
         <div className={styles.calcTasksWrapper}>
-                <button
-                    className={classnames({
-                        [styles.button]: true,
-                        [styles.selected]: filtered === 'Завершенные'
-                    })}
-                    onClick={() => onClickFilteredTasks('Завершенные')}
-                >
-                    Сделано:
-                    <span className={styles.tasksCount}>
-            {completedTasks}
-          </span>
-                </button>
-                <button
-                    className={classnames({
-                        [styles.button]: true,
-                        [styles.selected]: filtered === 'Незавершенные'
-                    })}
-                    onClick={() => onClickFilteredTasks('Незавершенные')}
-                >
-                    Осталось:
-                    <span className={styles.tasksCount}>
-            {uncompletedTasks}
-          </span>
-                </button>
-                <button
-                    className={classnames({
-                        [styles.button]: true,
-                        [styles.selected]: filtered === 'Все'
-                    })}
-                    onClick={()=>onClickFilteredTasks('Все')}
-                >
-                    Все:
-                    <span className={styles.tasksCount}>
-            {allTasks}
-          </span>
-                </button>
-            </div>
+          <button
+            className={classnames({
+              [styles.button]: true,
+              [styles.selected]: filtered === 'Завершенные'
+            })}
+            onClick={() => onClickFilteredTasks('Завершенные')}
+          >
+            Сделано:
+            <span className={styles.tasksCount}>
+              {completedTasks}
+            </span>
+          </button>
+          <button
+            className={classnames({
+              [styles.button]: true,
+              [styles.selected]: filtered === 'Незавершенные'
+            })}
+            onClick={() => onClickFilteredTasks('Незавершенные')}
+          >
+            Осталось:
+            <span className={styles.tasksCount}>
+              {uncompletedTasks}
+            </span>
+          </button>
+          <button
+            className={classnames({
+              [styles.button]: true,
+              [styles.selected]: filtered === 'Все'
+            })}
+            onClick={()=>onClickFilteredTasks('Все')}
+          >
+            Все:
+              <span className={styles.tasksCount}>
+                {allTasks}
+              </span>
+          </button>
+        </div>
     )
 
 }
 
 export default CalcTodo;
-
--- src/Components/CalcTodo/CalcTodo.test.jsx
+++ src/Components/CalcTodo/CalcTodo.test.jsx
@@ -2,16 +2,55 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import CalcTodo from "./CalcTodo";
 
-test('case', ()=> {
-  const props = {
-    tasks: [
-      {value:"Изучить React", isDone:false, isImportant: true, id:1},
-      {value:"Найти работу",isDone:false, isImportant: true,id:2}
-    ],
-    onClickFilteredTasks:'Все',
-    filtered:'Все'
-  };
+describe('CalcTodo тесты', ()=>{
+    const props = {
+      tasks: [
+        {value:"Изучить React", isDone:false, isImportant: true, id:1},
+        {value:"Найти работу",isDone:false, isImportant: true,id:2}
+      ],
+      onClickFilteredTasks:'Все',
+      filtered:'Все'
+    };
+  describe('Проверяем исходное состояние',()=>{
+    const wrapper = shallow(<CalcTodo tasks={props.tasks}/>);
+    test('Создан компонент CalcTodo', ()=> {
+      expect(wrapper.exists()).toBe(true);
+    });
+    test('Создано 3 кнопки', ()=> {
+      expect(wrapper.find('button')).toHaveLength(3);
+    });
+    test('Кнопка "все" имеет текст = 2', ()=> {
+      expect(wrapper.find('button').last().find('span').text()).toEqual('2');
+    });
+    test('Кнопка "сделано" имеет текст = 0', ()=> {
+      expect(wrapper.find('button').first().find('span').text()).toEqual('0');
+    });
+    test('Кнопка "осталось" имеет текст = 2', ()=> {
+      expect(wrapper.find('button').at('1').find('span').text()).toEqual('2');
+    });
+  })
 
-  const wrapper = shallow(<CalcTodo tasks={props.tasks}/>);
-  expect(wrapper.exists()).toBe(true);
+  describe('Увеличим колличество задач',()=>{
+    const nextProps = {
+      ...props,
+      tasks: [
+        {value:"Изучить React", isDone:false, isImportant: true, id:1},
+        {value:"Найти работу",isDone:false, isImportant: true,id:2},
+        {value:"Написать резюме",isDone:false, isImportant: true,id:3},
+        {value:"Сходить на собеседование",isDone:false, isImportant: true,id:4},
+      ]
+    };
+
+    const wrapper = shallow(<CalcTodo tasks={nextProps.tasks}/>);
+
+    test('Кнопка "все" имеет текст = 4', ()=> {
+      expect(wrapper.find('button').last().find('span').text()).toEqual('4');
+    });
+    test('Кнопка "сделано" имеет текст = 0', ()=> {
+      expect(wrapper.find('button').first().find('span').text()).toEqual('0');
+    });
+    test('Кнопка "осталось" имеет текст = 4', ()=> {
+      expect(wrapper.find('button').at('1').find('span').text()).toEqual('4');
+    });
+  })
 });
